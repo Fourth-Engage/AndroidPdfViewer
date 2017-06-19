9daa4cb770ba2ea33acddb49f2b5285952d3f2ca
Denis Santos <denis.santos@fourth.com>
2017-06-19 12:44:07 +0100

    - updated to read from local file - added try catch - corrected error, cleaned imports - added back share functionality for future ref - quick refactoring

--- pdfviewer-library/src/main/java/xyz/guutong/androidpdfviewer/PdfViewActivity.java
+++ pdfviewer-library/src/main/java/xyz/guutong/androidpdfviewer/PdfViewActivity.java
@@ -1,14 +1,11 @@
 package xyz.guutong.androidpdfviewer;
 
 import android.Manifest;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
-import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
@@ -18,20 +15,14 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
-import android.widget.Toast;
 
 import com.github.barteksc.pdfviewer.listener.OnLoadCompleteListener;
 import com.github.barteksc.pdfviewer.listener.OnPageChangeListener;
 import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.InputStream;
 
 import xyz.guutong.androidpdfviewer.Utils.DownloadFile;
-import xyz.guutong.androidpdfviewer.Utils.DownloadFileUrlConnectionImpl;
-import xyz.guutong.androidpdfviewer.Utils.FileUtil;
 
 public class PdfViewActivity extends AppCompatActivity implements DownloadFile.Listener, OnPageChangeListener, OnLoadCompleteListener {
 
@@ -98,47 +89,16 @@ public class PdfViewActivity extends AppCompatActivity implements DownloadFile.L
 
         progressBar.setVisibility(View.VISIBLE);
 
-        downloadPdf(pdfUrl);
+
+        checkPermissions();
     }
 
-    private void downloadPdf(String inPdfUrl) {
-        try {
-            DownloadFile downloadFile = new DownloadFileUrlConnectionImpl(this, new Handler(), this);
-            downloadFile.download(inPdfUrl, new File(this.getCacheDir(), FileUtil.extractFileNameFromURL(inPdfUrl)).getAbsolutePath());
-        } catch (Exception e) {
-            Toast.makeText(this, "Error!", Toast.LENGTH_SHORT).show();
-            finish();
+    private void checkPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 200);
+        } else {
+            getMyPdf();
         }
-    }
-
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        menu.clear();
-        if (showShareButton)
-            menu.add(0, MENU_SHARE, Menu.NONE, R.string.share)
-                    .setIcon(R.drawable.ic_share)
-                    .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
-        if (showCloseButton)
-            menu.add(0, 1, MENU_CLOSE, R.string.close)
-                    .setIcon(R.drawable.ic_close)
-                    .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
-        return super.onPrepareOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int i = item.getItemId();
-        if (i == MENU_CLOSE) {
-            finish();
-        } else if (i == MENU_SHARE) {
-
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 200);
-            } else {
-                saveFile();
-            }
-        }
-        return true;
     }
 
     @Override
@@ -146,71 +106,93 @@ public class PdfViewActivity extends AppCompatActivity implements DownloadFile.L
         switch (requestCode) {
             case 200: {
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    saveFile();
+                    getMyPdf();
                 }
                 return;
             }
         }
     }
 
-    private void saveFile(){
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-
+    private void getMyPdf() {
+        // Log.d("pdfUrl", pdfUrl);
+        // loading local pdf file
         try {
-            int bytesum = 0;
-            int byteread = 0;
-            File oldfile = new File(getCacheDir(), FileUtil.extractFileNameFromURL(pdfUrl));
+            File file = new File(pdfUrl);
+            pdfView.fromFile(file)
+                    .defaultPage(0)
+                    .onPageChange(this)
+                    .enableAnnotationRendering(true)
+                    .onLoad(this)
+                    .scrollHandle(scrollHandle)
+                    .swipeHorizontal(swipeHorizontal)
+                    .load();
 
-            InputStream inStream = new FileInputStream(oldfile.getAbsoluteFile());
-            FileOutputStream fs = new FileOutputStream(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/" + FileUtil.extractFileNameFromURL(pdfUrl));
-            byte[] buffer = new byte[1444];
-            while ((byteread = inStream.read(buffer)) != -1) {
-                bytesum += byteread;
-                fs.write(buffer, 0, byteread);
-            }
-            inStream.close();
-            fs.close();
-
-            builder.setMessage("File was saved in Downloads");
-
-        } catch (Exception e) {
-            builder.setMessage("Unable to save file.");
+        } catch (Exception error) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Could not open file.");
+            builder.show();
         }
+    }
 
-        builder.show();
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        menu.clear();
+        // TODO: show a share button, allow user to save/rename file to downloads, remove cache
+        // if (showShareButton)
+        //     menu.add(0, MENU_SHARE, Menu.NONE, R.string.share)
+        //             .setIcon(R.drawable.ic_share)
+        //             .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        // if (showCloseButton)
+        menu.add(0, 1, MENU_CLOSE, R.string.close)
+                .setIcon(R.drawable.ic_close)
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == MENU_CLOSE) {
+            finish();
+        }
+//        else if (itemId == MENU_SHARE) {
+//            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//            CharSequence[] itemsAlert = {"Copy link", "Open browser"};
+//
+//            builder.setItems(itemsAlert, new DialogInterface.OnClickListener() {
+//                @Override
+//                public void onClick(DialogInterface dialog, int itemIndex) {
+//                    final int COPY_LINK = 0;
+//                    final String label = "URL";
+//
+//                    if (itemIndex == COPY_LINK) {
+//                        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+//                        ClipData clip = ClipData.newPlainText(label, pdfUrl);
+//                        clipboard.setPrimaryClip(clip);
+//                        return;
+//                    }
+//
+//                    Intent intentBrowser = new Intent(Intent.ACTION_VIEW);
+//                    intentBrowser.setData(Uri.parse(pdfUrl));
+//                    startActivity(intentBrowser);
+//                }
+//            });
+//            builder.show();
+//
+//        }
+        return true;
     }
 
     @Override
     public void onSuccess(String url, String destinationPath) {
-        File pdf = new File(destinationPath);
-
-        pdfView.fromFile(pdf)
-                .defaultPage(0)
-                .onPageChange(this)
-                .enableAnnotationRendering(true)
-                .onLoad(this)
-                .scrollHandle(scrollHandle)
-                .swipeHorizontal(swipeHorizontal)
-                .load();
     }
 
     @Override
     public void onFailure(Exception e) {
-        progressBar.setVisibility(View.GONE);
-        AlertDialog.Builder alert = new AlertDialog.Builder(this)
-                .setMessage("Cannot open file!")
-                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        finish();
-                    }
-                })
-                .setIcon(android.R.drawable.ic_dialog_alert);
-        alert.show();
     }
 
     @Override
     public void onProgressUpdate(int progress, int total) {
-
     }
 
     @Override
@@ -220,6 +202,5 @@ public class PdfViewActivity extends AppCompatActivity implements DownloadFile.L
 
     @Override
     public void onPageChanged(int page, int pageCount) {
-
     }
 }
